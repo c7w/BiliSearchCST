bbacb4f5609309dff008eb6b1ee47a26af4b6bb9
c7w <admin@cc7w.cf>
2021-09-01 10:51:36 +0800

    zip batch files

--- Crawler/Bash/CrawlerBash/1.bat
+++
@@ -1,4 +0,0 @@
-cd E:\Project\Homework\python\Crawler
-REM python main.py 3005000 3006499
-REM python main.py 5005000 5006499
-python main.py 8006652 8008151
--- Crawler/Bash/CrawlerBash/3.bat
+++
@@ -1,4 +0,0 @@
-cd E:\Project\Homework\python\Crawler
-REM python main.py 50007024 50008523
-REM python main.py 70007025 70008524
-python main.py 90007025 90008524
